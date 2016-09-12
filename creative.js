f812244218583b264af42e37a3c300b2e24c09ba
Brandon <bvance800@gmail.com>
2016-09-12 23:34:16 +0000

    The square moves and is updated 50 times a second

--- creative.js
+++ creative.js
@@ -1,38 +1,61 @@
 
 
 var myGamePiece;
+var WIDTH = 400;
+var HEIGHT = 400;
 
+var game = {	
 
-var game = {
+	canvas : document.createElement("canvas"),
 
 	start : function(){
-		alert("First alert game.start()");
-		this.canvas = document.getElementById("game-screen");
+		//alert("start()");
+		this.canvas.width = WIDTH;
+		this.canvas.height = HEIGHT;
+		this.canvas.id = "game-screen";
 		this.context = this.canvas.getContext("2d");
-		
-		alert("game.start()");
-		
+		document.body.insertBefore(this.canvas, document.body.childNodes[2]);		
+		this.interval = setInterval(updateGame, 20);
+	},
+	
+	clear : function(){
+	
+		this.context.clearRect(0,0,WIDTH, HEIGHT);		
+
 	}
+	
 }
 
 function startGame(){
 	
-	alert("startGame()");
-	game.start()
-	alert("second alert startGame()");
-	myGamePiece = new gamePeice(30, 30, "red", 50, 50);
+	//alert("startGame()");
+	game.start();
+	//alert("second alert startGame()");
+	myGamePiece = new gamePiece(30, 30, "red", 50, 50);
 
 }
 
-function gamePeice(width, height, color, x, y)
+function gamePiece(width, height, color, x, y)
 {
 
 	this.width = width;
 	this.height = height;
 	this.x = x;
 	this.y = y;
-	game.context.fillStyle = color;
-	game.context.fillRect(this.x, this.y, this.width, this.height);
 
+	this.update = function(){
+				
+		game.context.fillStyle = color;
+		game.context.fillRect(this.x, this.y, this.width, this.height);
+
+
+	}
 }
 
+function updateGame()
+{
+	game.clear();
+	myGamePiece.x += 1;
+	myGamePiece.update();
+	
+}
