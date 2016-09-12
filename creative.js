61c2ef42a8af16cfb4e8b8494dd844ce899446d0
Brandon <bvance800@gmail.com>
2016-09-12 18:06:02 +0000

    Game screen set up with a single red square drawn with javascript.

--- creative.js
+++ creative.js
@@ -1,1 +1,38 @@
 
+
+var myGamePiece;
+
+
+var game = {
+
+	start : function(){
+		alert("First alert game.start()");
+		this.canvas = document.getElementById("game-screen");
+		this.context = this.canvas.getContext("2d");
+		
+		alert("game.start()");
+		
+	}
+}
+
+function startGame(){
+	
+	alert("startGame()");
+	game.start()
+	alert("second alert startGame()");
+	myGamePiece = new gamePeice(30, 30, "red", 50, 50);
+
+}
+
+function gamePeice(width, height, color, x, y)
+{
+
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;
+	game.context.fillStyle = color;
+	game.context.fillRect(this.x, this.y, this.width, this.height);
+
+}
+
